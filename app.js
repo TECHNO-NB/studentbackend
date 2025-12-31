36dd061192b12d621e5f908c7ff415a030a64536
NareshBhattarai4 <technologyandapplication@gmail.com>
2025-12-31 20:08:49 +0545

    fixed bug

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ limit: "5mb", extended: true }));
+app.use(express.json({ limit: "3mb" }));
+app.use(express.urlencoded({ limit: "3mb", extended: true }));
 
 // Logging (dev only)
 if (process.env.NODE_ENV === "development") {
